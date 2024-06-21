5d58e4356a3549c7d55bffe53e16eff499bb1834
Andrei M <moisei555@yahoo.com>
2024-06-21 15:53:31 +0500

    урок 1

--- src/main/java/org/example/HomeWorkApp.java
+++ src/main/java/org/example/HomeWorkApp.java
@@ -1,42 +1,44 @@
 package org.example;
 // урок 1
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign(10, 20);
-        printColor(1);
-        compareNumbers(30,60);
+        printColor(100);
+        compareNumbers(30, 60);
     }
     private static void printThreeWords() { //Создать метод который выводит все слова
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-    private static void checkSumSign(int a, int b){ // Создать метод который должен проссуммировать переменные и вывести положит или отр результат
+    private static void checkSumSign(int a, int b) { // Создать метод который должен проссуммировать переменные и вывести положит или отр результат
         int c = a + b;
         if (c >= 0) {
             System.out.println("положительный");
         } else {
             System.out.println("отрицательный");
         }
-    }     // Создать метод который будет выводить текст цвета в зависимости от числа
+    }// Создать метод который будет выводить текст цвета в зависимости от числа
     private static void printColor(int value) {
         if (value <= 0) {
             System.out.println("Красный");
-        }
-        if (value > 0 && value < 100) {
+        }else if (value > 0 && value < 100){
             System.out.println("желтый");
-        }else{
+        }else {
             System.out.println("зеленый");
         }
-            } // Создать метод больше меньше или равно
+    }// Создать метод больше меньше или равно
+
     private static void compareNumbers(int a, int b) {
         if (a >= b) {
             System.out.println("a => b");
-        }
-        if ( a < b) {
-            System.out.println("a < b");}
+        } else {
+            System.out.println("a < b");
         }
     }
+}
 
