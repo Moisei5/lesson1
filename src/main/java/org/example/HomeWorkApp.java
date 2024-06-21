734e6c9ae6dc8e5997cb40ffa8e56055d56834a0
Andrei M <moisei555@yahoo.com>
2024-06-21 14:54:34 +0500

    урок 1

--- src/main/java/org/example/HomeWorkApp.java
+++ src/main/java/org/example/HomeWorkApp.java
@@ -4,18 +4,16 @@ package org.example;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        checkSumSign(10, 20);
+        printColor(1);
+        compareNumbers(30,60);
     }
     private static void printThreeWords() { //Создать метод который выводит все слова
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-    private static void checkSumSign() { // Создать метод который должен проссуммировать переменные и вывести положит или отр результат
-        int a = 10;
-        int b = 20;
+    private static void checkSumSign(int a, int b){ // Создать метод который должен проссуммировать переменные и вывести положит или отр результат
         int c = a + b;
         if (c >= 0) {
             System.out.println("положительный");
@@ -23,20 +21,17 @@ public class HomeWorkApp {
             System.out.println("отрицательный");
         }
     }     // Создать метод который будет выводить текст цвета в зависимости от числа
-    private static void printColor() {
-        int value = 101;
+    private static void printColor(int value) {
         if (value <= 0) {
             System.out.println("Красный");
         }
-        if (value > 0 && value <100){
-        System.out.println("желтый");
-    }
-        if (value > 100) {
+        if (value > 0 && value < 100) {
+            System.out.println("желтый");
+        }else{
             System.out.println("зеленый");
         }
             } // Создать метод больше меньше или равно
-    private static void compareNumbers() {
-        int a = 30, b = 60;
+    private static void compareNumbers(int a, int b) {
         if (a >= b) {
             System.out.println("a => b");
         }
